29b83804541c06b6e4a83773a5dee4d13f23e35d
Manuel Gallina <manuel.gallina@3ds.team>
2025-01-29 09:24:58 +0100

    feat: add id default factory method

--- src/app/core/models/_base.py
+++ src/app/core/models/_base.py
@@ -1,10 +1,11 @@
 from uuid import UUID
 
+import uuid6
 from sqlmodel import SQLModel, Field
 
 
 class BaseTable(SQLModel):
-    id: UUID = Field(primary_key=True)
+    id: UUID = Field(primary_key=True, default_factory=uuid6.uuid7)
 
     class Config:
         from_attributes = True
